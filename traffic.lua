6580c5a34f94ae427b2c42a7e8a2abfedf5e3f88
Blue <iamabluewolf@gmail.com>
2018-10-17 16:17:12 +0200

    improve wtf code

--- traffic.lua
+++ traffic.lua
@@ -6,14 +6,8 @@ Citizen.CreateThread(function()
 		SetRandomVehicleDensityMultiplierThisFrame(TrafficAmount/100)
 		SetParkedVehicleDensityMultiplierThisFrame(ParkedAmount/100)
 		SetScenarioPedDensityMultiplierThisFrame(PedestrianAmount/100, PedestrianAmount/100)
-		if not EnableDispatch then 
-			for i = 1, 13 do
-				EnableDispatchService(i, false)
-			end
-		else
-			for i = 1, 13 do
-				EnableDispatchService(i, true)
-			end
+		for i = 1, 13 do
+			EnableDispatchService(i, EnableDispatch)
 		end
 		Citizen.Wait(1)			
 	end
