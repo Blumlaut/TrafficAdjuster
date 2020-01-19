ecd3a8f710acaa3f12cf0f52bb4e1486d3c178a9
Blue <13604413+Bluethefurry@users.noreply.github.com>
2020-01-19 16:02:02 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,8 @@ TrafficAmount = 100
 PedestrianAmount = 100
 ParkedAmount = 100
 EnableDispatch = true
-
+EnableBoats = true
+EnableTrains = true
 
 --[[
 	TrafficAmount changes how much traffic there is on the Roads, this goes from 100% to 0%, values over 100% are also supported but not recommended, under 0% will cause a game crash.
@@ -15,7 +16,10 @@ EnableDispatch = true
 		- Ambulance Responses
 		- Roadblocks
 		- Gangs
-		
+	EnableBoats is a toggle between true and false, enables/disables random boats in the ocean.
+	EnableTrains is the same as above, with Trains
+
+
 
 
 ]]
