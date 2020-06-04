84eba59ab46e1e86b5e0e10d09c508f6f672321c
Fezz <43589668+FBFezz@users.noreply.github.com>
2020-06-04 19:28:33 +0100

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'rdr3', 'gta5' }
+
+author 'Bluethefurry'
+description 'TrafficAdjuster'
+version '1.0.0'
+
+client_scripts {
+    "config.lua",
+    "traffic.lua",
+}
